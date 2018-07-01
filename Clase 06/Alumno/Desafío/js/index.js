01f3acd0705610a2c9dce42e8b75801a01747567
Magdalena Monserrat <magui_monserrat@hotmail.com>
2018-07-01 17:22:53 -0300

    6

--- Clase 06/Alumno/Desafío/js/index.js
+++ Clase 06/Alumno/Desafío/js/index.js
@@ -19,20 +19,3 @@ console.log(includesText('paT', 'patricIA'))
 console.log(includesText('Patricia', 'Pa')) // Deberá devolver false
 
 console.log(includesText(2, 'Pa')) // Deberá devolver false
-
-// var numbers = [ 1, 2, 3, 4, 5 ]
-
-// var numbers2
-
-// function deleteElement (index, elements) {
-//   if (Array.isArray(elements)) {
-//     var numbers2 = numbers.slice()
-//     numbers2.splice(index, 1)
-//     return numbers2
-//   } else {
-//     return elements
-//   }
-// }
-
-// console.log('Array copia ', deleteElement(2, numbers))
-// console.log('Array original ', numbers)
